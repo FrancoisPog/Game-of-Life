c0c27f3ed41617b1311116e1ddf64a2415ac195d
François Poguet <francoispoguet@gmail.com>
2020-10-01 16:50:56 +0200

    game playable step/step and auto

--- jeudelavie.js
+++ jeudelavie.js
@@ -1,29 +1,63 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-    const GRID_SIZE = 5;
+    const GRID_SIZE = 20;
     const CANVAS_SIZE = 600;
+
+
+    /**
+     * Test if two matrix are equals
+     * @param {Array[]} mat1
+     * @param {Array[]} mat2
+     * @returns {boolean}
+     */
+    function matrixEquals(mat1,mat2){
+        for(let i in mat1){
+            for(let j in mat1[i] ){
+                if(mat1[i][j] !== mat2[i][j]){
+                    return false
+                }
+            }
+        }
+
+        return true;
+    }
 
     class Game {
         constructor() {
-            this.matrix = new Array(GRID_SIZE);
+            this.state = {
+                memento: [],
+                bornCellColor: "green",
+                deadCellColor: "red"
+            }
+            let matrix = new Array(GRID_SIZE);
 
-            for (let i = 0; i < this.matrix.length; ++i) {
-                this.matrix[i] = new Array(GRID_SIZE).fill(false);
+            for (let i = 0; i < matrix.length; ++i) {
+                matrix[i] = new Array(GRID_SIZE).fill(false);
             }
 
+            this.state.memento.push(matrix);
 
 
+            // TODO : take the canvas out of the class
             this.canvas = document.getElementById("cvs");
             this.cx = this.canvas.getContext("2d");
 
             this.canvas.onmouseleave = () => this.updateDisplay();
-
             this.canvas.onmousemove = (e) => this.drawHoverRect(e);
-
             this.canvas.onclick = (e) => this.addPoint(e);
+
         }
 
-        addPoint(e) {
+        get matrix() {
+            return this.state.memento[this.state.memento.length - 1];
+        }
+
+
+
+        addPoint(e, canvas) {
+            if(runningInterval != null){
+                return;
+            }
             let rect = e.target.getBoundingClientRect();
             let x = e.clientX - rect.left;
             let y = e.clientY - rect.top;
@@ -38,7 +72,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
 
-        drawHoverRect(e) {
+        drawHoverRect(e, canvas) {
+            if(runningInterval != null){
+                return;
+            }
+
             this.updateDisplay();
             let rect = e.target.getBoundingClientRect();
             let x = e.clientX - rect.left;
@@ -47,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
             let cellX = x - (x % (CANVAS_SIZE / GRID_SIZE));
             let cellY = y - (y % (CANVAS_SIZE / GRID_SIZE));
 
-            console.log(cellY, cellX);
+            // TODO : check if the x & y are in the matrix
             if (this.matrix[cellY / (CANVAS_SIZE / GRID_SIZE)][cellX / (CANVAS_SIZE / GRID_SIZE)]) {
                 return;
             }
@@ -56,7 +94,8 @@ document.addEventListener("DOMContentLoaded", () => {
             this.cx.fillRect(cellX, cellY, CANVAS_SIZE / GRID_SIZE, CANVAS_SIZE / GRID_SIZE);
         }
 
-        updateDisplay() {
+        updateDisplay(canvas) {
+
             this.cx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
             this.matrix.forEach((row, i) =>
                 row.forEach((cell, j) => {
@@ -72,7 +111,93 @@ document.addEventListener("DOMContentLoaded", () => {
                 })
             );
         }
+
+        countNeighbors(x, y) {
+            let res = 0;
+            for (let i = x - 1; i <= x + 1; ++i) {
+                for (let j = y - 1; j <= y + 1; ++j) {
+                    if (i >= 0 &&
+                        j >= 0 &&
+                        i < this.matrix.length &&
+                        j < this.matrix.length &&
+                        this.matrix[i][j] &&
+                        !(j === y && i === x)
+                    ) {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+        next() {
+            console.log('update');
+            let newMatrix = [];
+            for (let i = 0; i < this.matrix.length; ++i) {
+                newMatrix[i] = [];
+                for (let j = 0; j < this.matrix.length; ++j) {
+                    let nbNeighbors = this.countNeighbors(i, j);
+                    if (this.matrix[i][j]) {
+                        newMatrix[i][j] = nbNeighbors === 2 || nbNeighbors === 3;
+                    } else {
+                        newMatrix[i][j] = nbNeighbors === 3;
+                    }
+                }
+            }
+
+            if(matrixEquals(this.matrix,newMatrix)){
+                console.log('stable');
+                return false;
+            }
+
+            this.state.memento.push(newMatrix);
+            this.updateDisplay();
+            return true;
+        }
+
+        previous(){
+            if(this.state.memento.length === 1){
+                return;
+            }
+            this.state.memento.pop();
+            this.updateDisplay();
+        }
+
+
     }
 
-    new Game();
-});
+    let game = new Game();
+    let runningInterval = null;
+
+    document.getElementById('btnNext').onclick = () => {
+        if(runningInterval){
+            return;
+        }
+        game.next()
+    }
+    document.getElementById('btnPlay').onclick = () => {
+        if (runningInterval) {
+            return;
+        }
+        runningInterval = setInterval(() => {
+            if(game.next() === false){
+                clearInterval(runningInterval);
+                runningInterval = null;
+            }
+        }, 300);
+    }
+
+    document.getElementById('btnStop').onclick = () => {
+        clearInterval(runningInterval);
+        runningInterval = null;
+    }
+
+    document.getElementById('btnReset').onclick = () => {
+        if(runningInterval){
+            return;
+        }
+        game.previous();
+    }
+
+})
+;
