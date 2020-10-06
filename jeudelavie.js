bc803ea2ec3826c66b577ce1984cd3273bf86221
François Poguet <francoispoguet@gmail.com>
2020-10-06 09:44:07 +0200

    Refactoring

--- jeudelavie.js
+++ jeudelavie.js
@@ -1,8 +1,7 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-    const GRID_SIZE = 30;
-    const CANVAS_SIZE = 600;
 
+    let memento = [];
 
     /**
      * Test if two matrix are equals
@@ -22,182 +21,194 @@ document.addEventListener("DOMContentLoaded", () => {
         return true;
     }
 
-    class Game {
-        /**
-         * Create a new game
-         * @param {HTMLCanvasElement} canvas
-         */
-        constructor(canvas) {
+    /**
+     * Get the current matrix
+     * @returns {Array[]}
+     */
+    function CurrentMatrix() {
+        return memento[memento.length - 1];
+    }
 
-            this.memento = []
+    /**
+     * Initialize a matrix and reset the memento
+     * @param size
+     */
+    function initMatrix(size = GRID_SIZE) {
+        let matrix = new Array(size);
 
-            let matrix = new Array(GRID_SIZE);
-            for (let i = 0; i < matrix.length; ++i) {
-                matrix[i] = new Array(GRID_SIZE).fill(false);
-            }
-
-            this.memento.push(matrix);
-
-
-            this.canvas = canvas;
-            this.cx = this.canvas.getContext('2d');
-
-            this.canvas.onmouseleave = () => this.updateDisplay();
-            this.canvas.onmousemove = (e) => this.drawHoverRect(e);
-            this.canvas.onclick = (e) => this.addPoint(e);
-
+        for (let i = 0; i < matrix.length; ++i) {
+            matrix[i] = new Array(size).fill(false);
         }
 
-        get matrix() {
-            return this.memento[this.memento.length - 1];
-        }
-
-
-        /**
-         * Add a point in the matrix
-         * @param e The mouse click event
-         */
-        addPoint(e) {
-            if (runningInterval != null) {
-                return;
-            }
-            let rect = e.target.getBoundingClientRect();
-            let x = e.clientX - rect.left;
-            let y = e.clientY - rect.top;
-
-
-            let rowIndex = Math.floor(y / (CANVAS_SIZE / GRID_SIZE));
-            let cellIndex = Math.floor(x / (CANVAS_SIZE / GRID_SIZE));
-
-            this.matrix[rowIndex][cellIndex] = true;
-
-            this.updateDisplay();
-        }
-
-
-        /**
-         * Fill the hovered cell by grey
-         * @param e The mouse over event
-         */
-        drawHoverRect(e) {
-            if (runningInterval != null) {
-                return;
-            }
-            this.updateDisplay();
-
-            let rect = e.target.getBoundingClientRect();
-            let x = e.clientX - rect.left;
-            let y = e.clientY - rect.top;
-
-            let cvsX = x - (x % (CANVAS_SIZE / GRID_SIZE));
-            let cvsY = y - (y % (CANVAS_SIZE / GRID_SIZE));
-
-            let cellX  = cvsX / (CANVAS_SIZE / GRID_SIZE);
-            let cellY = cvsY / (CANVAS_SIZE / GRID_SIZE)
-
-            if(cellY >= GRID_SIZE || cellX >= GRID_SIZE || this.matrix[cellY][cellX]){
-                return;
-            }
-
-
-            this.cx.fillStyle = "#a0a0a0";
-            this.cx.fillRect(cvsX, cvsY, CANVAS_SIZE / GRID_SIZE, CANVAS_SIZE / GRID_SIZE);
-        }
-
-        /**
-         * Update the display in the canvas
-         */
-        updateDisplay() {
-            this.cx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-            this.matrix.forEach((row, i) =>
-                row.forEach((cell, j) => {
-                    if (cell) {
-                        this.cx.fillStyle = "black";
-                        this.cx.fillRect(
-                            j * (CANVAS_SIZE / GRID_SIZE),
-                            i * (CANVAS_SIZE / GRID_SIZE),
-                            CANVAS_SIZE / GRID_SIZE,
-                            CANVAS_SIZE / GRID_SIZE
-                        );
-                    }
-                })
-            );
-        }
-
-        /**
-         * Count the number of living cell in the neighbors
-         * @param x
-         * @param y
-         * @returns {number}
-         */
-        countNeighbors(x, y) {
-            let res = 0;
-            for (let i = x - 1; i <= x + 1; ++i) {
-                for (let j = y - 1; j <= y + 1; ++j) {
-                    if (i >= 0 &&
-                        j >= 0 &&
-                        i < this.matrix.length &&
-                        j < this.matrix.length &&
-                        this.matrix[i][j] &&
-                        !(j === y && i === x)
-                    ) {
-                        res++;
-                    }
-                }
-            }
-            return res;
-        }
-
-        /**
-         * Move on the next generation
-         * @returns {boolean}
-         */
-        next() {
-            let newMatrix = [];
-            for (let i = 0; i < this.matrix.length; ++i) {
-                newMatrix[i] = [];
-                for (let j = 0; j < this.matrix.length; ++j) {
-                    let nbNeighbors = this.countNeighbors(i, j);
-                    if (this.matrix[i][j]) {
-                        newMatrix[i][j] = nbNeighbors === 2 || nbNeighbors === 3;
-                    } else {
-                        newMatrix[i][j] = nbNeighbors === 3;
-                    }
-                }
-            }
-
-            if (matrixEquals(this.matrix, newMatrix)) {
-                //console.log('stable');
-                return false;
-            }
-
-            this.memento.push(newMatrix);
-            this.updateDisplay();
-            return true;
-        }
-
-        /**
-         * Back to the previous generation
-         */
-        previous() {
-            if (this.memento.length === 1) {
-                return;
-            }
-            this.memento.pop();
-            this.updateDisplay();
-        }
-
+        memento = [matrix];
 
     }
 
-    let game = new Game(document.getElementsByTagName('canvas')[0]);
+
+    /**
+     * Add a point in the matrix
+     * @param e The mouse click event
+     */
+    function addPoint(e) {
+        if (runningInterval != null) {
+            return;
+        }
+        let rect = e.target.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+
+        let rowIndex = Math.floor(y / (CANVAS_SIZE / GRID_SIZE));
+        let cellIndex = Math.floor(x / (CANVAS_SIZE / GRID_SIZE));
+
+        CurrentMatrix()[rowIndex][cellIndex] = true;
+
+        updateDisplay();
+    }
+
+
+    /**
+     * Fill the hovered cell by grey
+     * @param e The mouse over event
+     */
+    function drawHoverRect(e) {
+        if (runningInterval != null) {
+            return;
+        }
+        updateDisplay();
+
+        let rect = e.target.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        let cvsX = x - (x % (CANVAS_SIZE / GRID_SIZE));
+        let cvsY = y - (y % (CANVAS_SIZE / GRID_SIZE));
+
+        let cellX = cvsX / (CANVAS_SIZE / GRID_SIZE);
+        let cellY = cvsY / (CANVAS_SIZE / GRID_SIZE)
+
+        if (cellY >= GRID_SIZE || cellX >= GRID_SIZE || CurrentMatrix()[cellY][cellX]) {
+            return;
+        }
+
+
+        cx.fillStyle = "#a0a0a0";
+        cx.fillRect(cvsX, cvsY, CANVAS_SIZE / GRID_SIZE, CANVAS_SIZE / GRID_SIZE);
+    }
+
+    /**
+     * Update the display in the canvas
+     */
+    function updateDisplay() {
+        let matrix = CurrentMatrix();
+
+        cx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+        matrix.forEach((row, i) =>
+            row.forEach((cell, j) => {
+                if (cell) {
+                    cx.fillStyle = "black";
+                    cx.fillRect(
+                        j * (CANVAS_SIZE / GRID_SIZE),
+                        i * (CANVAS_SIZE / GRID_SIZE),
+                        CANVAS_SIZE / GRID_SIZE,
+                        CANVAS_SIZE / GRID_SIZE
+                    );
+                }
+            })
+        );
+    }
+
+    /**
+     * Count the number of living cell in the neighbors
+     * @param x
+     * @param y
+     * @returns {number}
+     */
+    function countNeighbors(x, y) {
+        let res = 0;
+        let matrix = CurrentMatrix();
+        for (let i = x - 1; i <= x + 1; ++i) {
+            for (let j = y - 1; j <= y + 1; ++j) {
+                if (i >= 0 &&
+                    j >= 0 &&
+                    i < matrix.length &&
+                    j < matrix.length &&
+                    matrix[i][j] &&
+                    !(j === y && i === x)
+                ) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Move on the next generation
+     * @returns {boolean}
+     */
+    function next() {
+        let newMatrix = [];
+        let matrix = CurrentMatrix();
+        for (let i = 0; i < matrix.length; ++i) {
+            newMatrix[i] = [];
+            for (let j = 0; j < matrix.length; ++j) {
+                let nbNeighbors = countNeighbors(i, j);
+                if (matrix[i][j]) {
+                    newMatrix[i][j] = nbNeighbors === 2 || nbNeighbors === 3;
+                } else {
+                    newMatrix[i][j] = nbNeighbors === 3;
+                }
+            }
+        }
+
+        if (matrixEquals(CurrentMatrix(), newMatrix)) {
+            return false;
+        }
+
+        memento.push(newMatrix);
+        updateDisplay();
+
+        return true;
+    }
+
+    /**
+     * Back to the previous generation
+     */
+    function previous() {
+        if (memento.length === 1) {
+            return;
+        }
+        memento.pop();
+        updateDisplay();
+    }
+
+
+    //******************************************
+    //                  SCRIPT
+    //******************************************
+
+    const GRID_SIZE = 30;
+    const CANVAS_SIZE = 600;
+
+    initMatrix(GRID_SIZE);
+
+    /*** @type {HTMLCanvasElement} */
+    let canvas = document.getElementsByTagName('canvas')[0];
+    let cx = canvas.getContext('2d');
+
+    canvas.onmouseleave = () => updateDisplay();
+    canvas.onmousemove = (e) => drawHoverRect(e);
+    canvas.onclick = (e) => addPoint(e);
+
     let runningInterval = null;
 
     document.getElementById('btnNext').onclick = () => {
         if (runningInterval) {
             return;
         }
-        game.next()
+        next()
     }
 
     document.getElementById('btnPlay').onclick = () => {
@@ -205,7 +216,7 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
         runningInterval = setInterval(() => {
-            if (game.next() === false) {
+            if (next() === false) {
                 clearInterval(runningInterval);
                 runningInterval = null;
             }
@@ -221,7 +232,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (runningInterval) {
             return;
         }
-        game.previous();
+        previous();
     }
 
 });
